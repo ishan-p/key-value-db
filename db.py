4ebbc4cf9d6aaa835b3bb7a3cdc181362616184b
Ishan <ishan@shoptimize.in>
2021-04-02 09:32:10 +0530

    Add a db service class
    Consumer will use this service class to interacct with the database.

---
+++ db.py
@@ -0,0 +1,32 @@
+from handler import DbHandler
+
+
+class Db(object):
+
+    def __init__(self):
+        self.handle = DbHandler()
+
+    def set(self, key, value):
+        record = self.handle.create_or_update(key, value)
+        return record.value
+
+    def get(self, key):
+        try:
+            record = self.handle.read(key)
+            return record.value
+        except FileNotFoundError:
+            return None
+
+    def delete(self, key):
+        self.handle.delete(key)
+
+    def increment(self, key):
+        record = self.handle.increment(key)
+        return record.value
+
+    def increment_by(self, key, increment_by):
+        try:
+            record = self.handle.increment_by(key, increment_by)
+            return record.value
+        except FileNotFoundError:
+            return None
